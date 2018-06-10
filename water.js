d1569286a59383946194e36b644a89a064c86dd4
jy <39288115+izAL17x9iZmxoo423gvY@users.noreply.github.com>
2018-06-10 16:22:36 +0800

    Update water.js

--- water.js
+++ water.js
@@ -53,7 +53,6 @@ client.on('message', message => {
   }
 }
     if (message.content === `${prefix}help`) {
-       if (!message.channel.type === 'dm') {
         message.channel.send({embed : {
           color: 0x7CFC00,
       title: "Help is on the way!",
@@ -139,18 +138,6 @@ client.on('message', message => {
           }
         });
        }
-        else
-            message.channel.send({embed : {
-        color: 0xFF0000,
-    title: "An error occured!",
-    description: `Commands cannot be used in Direct Messages!`,
-    timestamp: false,
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Water Bot"
-    }
-    }
-  });
     }
   else if (message.content.startsWith(`${prefix}8ball`)) {
       const args = message.content.slice(prefix.length).trim().split(/ +/g);
